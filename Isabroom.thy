d4fa9f764fd876ce1bde9811b9d3a16e31070f32
Florian Sextl <florian.sextl@tuwien.ac.at>
2022-09-08 17:08:26 +0200

    status 2022-09-08

--- Isabroom.thy
+++ Isabroom.thy
@@ -12,7 +12,7 @@ datatype 'a command =
 | AssignVar var var
 | Load var var
 | Store var var
-| Malloc var
+| Malloc var var
 | Free var
 | BinOp binop var var
 | UnOp unop var
@@ -31,14 +31,55 @@ axiomatization entry :: CFG_node and exit :: CFG_node
 type_synonym 'a edge = "CFG_node \<times> 'a command \<times> CFG_node"
 type_synonym 'a cfg = "'a func \<times> 'a edge set"
 
+abbreviation can_allocate_from :: "blocks \<Rightarrow> loc \<Rightarrow> blockname \<Rightarrow> val \<Rightarrow> bool" where
+  "can_allocate_from B l n z \<equiv>
+    0<l \<and> \<comment> \<open>The location is not at null.\<close>
+    l < l + (loc64_ops.from_val z) \<and> \<comment> \<open>The allocated block does not wrap around.\<close>
+    \<comment> \<open>It doesn't overlap with any other block.\<close>
+    (\<forall>b\<in>set (raw_blocks B). \<not>(overlaps_with (n,l,l + (loc64_ops.from_val z)) b)) \<and>
+    (\<forall>b\<in>set (raw_blocks B). (case b of (n',_,_) \<Rightarrow> n'\<noteq>n)) \<comment> \<open>The name is new and unique\<close>"
+
 inductive semantics :: "pre_config \<Rightarrow> 'a command \<Rightarrow> pre_config option \<Rightarrow> bool" where
-  "fits k x \<Longrightarrow> semantics (S,B,M) (AssignConst x k) (Some (Stack ((lookup s)(x:=k)),B,M))"
-| "size x = size y \<Longrightarrow> semantics (S,B,M) (AssignVar x y) (Some (Stack ((lookup s)(x:=lookup s y)),B,M))"
-| "base B (loc64_ops.from_val (lookup s y)) = 0 \<or> 
-  (loc64_ops.from_val (lookup s y)) + (sizev x) > ende B (loc64_ops.from_val (lookup s y)) \<Longrightarrow>
-   semantics (S,B,M) (Load x y) None"
-| "\<not>(base B (loc64_ops.from_val (lookup s y)) = 0 \<or> 
-  (loc64_ops.from_val (lookup s y)) + (sizev x) > ende B (loc64_ops.from_val (lookup s y))) \<Longrightarrow>
-  
-  semantics (Stack ((lookup s)(x:=
+  "fits k x \<Longrightarrow> semantics (S,B,M) (AssignConst x k) (Some (Stack ((lookup S)(x:=k)),B,M))"
+| "size x = size y \<Longrightarrow> semantics (S,B,M) (AssignVar x y) (Some (Stack ((lookup S)(x:=lookup S y)),B,M))"
+| LoadFail: 
+  "\<comment> \<open>y does not point into any block, i.e. is an invalid location\<close>
+  base B (loc64_ops.from_val (lookup S y)) = 0 \<or>
+  \<comment> \<open>This load would read past the end of the block.\<close>
+  ((loc64_ops.from_val (lookup S y)) + (sizev x) > ende B (loc64_ops.from_val (lookup S y)) \<or> 
+  \<comment> \<open>This would read past the maximum location and wrap around.\<close>
+  (loc64_ops.from_val (lookup S y)) + (sizev x) \<le> (loc64_ops.from_val (lookup S y))) \<Longrightarrow>
+     semantics (S,B,M) (Load x y) None"
+| LoadSuccess: "\<lbrakk>\<not>(base B (loc64_ops.from_val (lookup S y)) = 0 \<or> 
+  ((loc64_ops.from_val (lookup S y)) + (sizev x) > ende B (loc64_ops.from_val (lookup S y)) \<or>
+  (loc64_ops.from_val (lookup S y)) + (sizev x) \<le> (loc64_ops.from_val (lookup S y))));
+  \<comment> \<open>The addressed memory cells are actually allocated.\<close>
+  access M (loc64_ops.from_val (lookup S y)) (size x) = Some v\<rbrakk> \<Longrightarrow> 
+    semantics (S,B,M) (Load x y) (Some (Stack ((lookup S)(x:=v)), B, M))"
+| StoreFail:
+  "\<comment> \<open>x does not point into any block, i.e. is an invalid location\<close>
+  base B (loc64_ops.from_val (lookup S x)) = 0 \<or>
+  \<comment> \<open>This store woudl write past the end of the block.\<close>
+  ((loc64_ops.from_val (lookup S x)) + (sizev y) > ende B (loc64_ops.from_val (lookup S x)) \<or> 
+  \<comment> \<open>This would write past the maximum location and wrap around.\<close>
+  (loc64_ops.from_val (lookup S x)) + (sizev y) \<le> (loc64_ops.from_val (lookup S x))) \<Longrightarrow>
+    semantics (S,B,M) (Store x y) None"
+| StoreSuccess: "\<not>(base B (loc64_ops.from_val (lookup S x)) = 0 \<or> 
+  ((loc64_ops.from_val (lookup S x)) + (sizev y) > ende B (loc64_ops.from_val (lookup S x)) \<or>
+  (loc64_ops.from_val (lookup S x)) + (sizev y) \<le> (loc64_ops.from_val (lookup S x)))) \<Longrightarrow> 
+    semantics (S,B,M) (Store x y) (Some (S, B, update_memory M (loc64_ops.from_val (lookup S x)) (lookup S y)))"
+| MallocError: "
+  \<comment> \<open>If we would allocate 0 bytes or x can't hold a pointer.\<close>
+  (\<forall>v\<in>set (lookup S z). v = 0) \<or> (size x \<noteq> loc_N_B) \<Longrightarrow> 
+  semantics (S,_,_) (Malloc x z) None"
+| MallocSuccessNull: "\<lbrakk>\<exists>v\<in>set (lookup S z). v \<noteq> 0; size x = loc_N_B\<rbrakk> \<Longrightarrow>
+  semantics (S,B,M) (Malloc x z) (Some (Stack ((lookup S)(x:=null x)),B,M))"
+| MallocSuccess: "\<lbrakk>\<exists>v\<in>set (lookup S z). v \<noteq> 0; size x = loc_N_B;
+  \<comment> \<open>l and n are new, k is arbitrary\<close>
+  can_allocate_from B l n (lookup S z); length k = length (lookup S z)\<rbrakk> \<Longrightarrow>
+  semantics (S,B,M) (Malloc x z) 
+    (Some (Stack ((lookup S)(x:=loc64_ops.to_val l)), 
+    Blocks ((n,l,l+(loc64_ops.from_val (lookup S z)))#raw_blocks B),
+    update_memory M l k))"
+
 end
--- Model.thy
+++ Model.thy
@@ -9,6 +9,12 @@ type_synonym loc = "64 word"
 abbreviation offset :: "loc \<Rightarrow> nat \<Rightarrow> loc" where
   "offset l n \<equiv> l + (word_of_nat n)"
 
+lemma offset_suc: "offset l (Suc n) = offset (offset l n) 1"
+  by (induction n) auto
+
+lemma offset_suc2: "offset l (Suc n) = offset (offset l 1) n"
+  by (induction n) auto
+
 locale loc_ops =
   fixes N :: "'a::len"
   assumes bytes: "LENGTH('a) mod 8 = 0" begin
@@ -34,6 +40,9 @@ lemma get_nth_byte_zero:
   "get_nth_byte 0 n = 0" by (simp add: get_nth_byte_def)
 end  
 
+abbreviation "loc_N_b\<equiv>64::nat"
+abbreviation "loc_N_B\<equiv>8::nat"
+
 interpretation loc64_ops: loc_ops "64::64" by standard simp
 
 typedecl var
@@ -41,6 +50,9 @@ axiomatization size :: "var \<Rightarrow> nat" where pos_size: "size v > 0"
 
 abbreviation sizev :: "var \<Rightarrow> 'a::len word" where
   "sizev v \<equiv> word_of_nat (size v)"
+
+definition null :: "var \<Rightarrow> val" where
+  "null x = replicate (size x) (0::byte)"
 
 definition fits :: "val \<Rightarrow> var \<Rightarrow> bool" where 
   "fits x v \<equiv> length x = size v"
@@ -72,35 +84,58 @@ lemma raw_access_set: "set (raw_access_rev m l n) = {m x | x. \<exists>n'. n' \<
 definition access :: "memory \<Rightarrow> loc \<Rightarrow> nat \<Rightarrow> val option" where
   "access m l n \<equiv> 
     fold (\<lambda>x l. case x of Some x' \<Rightarrow> (case l of Some l' \<Rightarrow> Some (x'#l') | _ \<Rightarrow> None) | _ \<Rightarrow> None) 
-      (rev (raw_access_rev m l n)) (Some [])"
+      (raw_access_rev m l n) (Some [])"
 
-lemma "access m l n \<noteq> None \<longleftrightarrow> (\<forall>x. l\<le>x \<and> x<offset l n \<longrightarrow> m x \<noteq> None)"
-proof (unfold access_def, induction "raw_access_rev m l n")
-  case Nil
-  with raw_access_set[of m l n] show ?case by auto
-next
-  case (Cons a x)
-  then show ?case sorry
-qed
-  
+lemma fold_neq_none:
+  "(fold (\<lambda>x l. case x of None \<Rightarrow> None | Some x' \<Rightarrow> case l of None \<Rightarrow> None | Some l' \<Rightarrow> Some (x' # l'))
+   l acc \<noteq> None) \<longleftrightarrow> ((\<forall>x\<in>set l. x \<noteq> None) \<and> acc \<noteq> None)"
+  apply (induction l arbitrary: acc)
+  by (auto split: option.splits)
+
+lemma fold_raw_access_none:
+  "(fold (\<lambda>x l. case x of None \<Rightarrow> None | Some x' \<Rightarrow> case l of None \<Rightarrow> None | Some l' \<Rightarrow> Some (x' # l'))
+   (raw_access_rev m l n) (Some acc) \<noteq> None) \<longleftrightarrow> (\<forall>y. (\<exists>n'. n' \<le> n \<and> y = offset l n') \<longrightarrow> m y \<noteq> None)" 
+  (is "(?f \<noteq> _) \<longleftrightarrow> _")
+proof -
+  have "?f \<noteq> None \<longleftrightarrow> (\<forall>x\<in>set (raw_access_rev m l n). x \<noteq> None)" using fold_neq_none by fastforce
+  also have "\<dots> = (\<forall>x\<in>{m y | y. \<exists>n'. n' \<le> n \<and> y = offset l n'}. x \<noteq> None)" by (simp add: raw_access_set)
+  also have "\<dots> = (\<forall>y. (\<exists>n'. n' \<le> n \<and> y = offset l n') \<longrightarrow> m y \<noteq> None)" by blast
+  finally show ?thesis .
+qed  
+
+lemma access_none: "access m l n \<noteq> None \<longleftrightarrow> (\<forall>y. (\<exists>n'. n' \<le> n \<and> y = offset l n') \<longrightarrow> m y \<noteq> None)" 
+  unfolding access_def using fold_raw_access_none by simp
+
+fun update_memory :: "memory \<Rightarrow> loc \<Rightarrow> val \<Rightarrow> memory" where
+  "update_memory m _ [] = m"
+| "update_memory m l (v#vs) = update_memory (m(l\<mapsto>v)) (offset l 1) vs"
+
 type_synonym blockname = nat
 type_synonym block = "(blockname\<times>(loc\<times>loc))"
 
 typedef blocks = "{b::block list. 
   (\<forall>n l u. (n,(l,u)) \<in> set b \<longrightarrow> (l<u \<and> l\<noteq>0)) \<and> 
   (\<forall>n1 n2 l1 l2 u1 u2. ((n1,(l1,u1)) \<in> set b \<and> (n2,(l2,u2)) \<in> set b 
-    \<and> (l1\<noteq>l2 \<or> u1 \<noteq> u2))
-    \<longrightarrow> ((u1\<le>l2 \<or> u2\<le>l1) \<and> n1\<noteq>n2))}" morphisms raw_blocks Blocks
+    \<and> (l1\<noteq>l2 \<or> u1 \<noteq> u2 \<or> n1\<noteq>n2))
+    \<longrightarrow> (u1\<le>l2 \<or> u2\<le>l1))}" morphisms raw_blocks Blocks
 proof -
   have "([]::block list) \<in> {b. (\<forall>n l u. (n, l, u) \<in> set b \<longrightarrow> l < u \<and> l \<noteq> 0) \<and>
-         (\<forall>n1 n2 l1 l2 u1 u2. (n1, l1, u1) \<in> set b \<and> (n2, l2, u2) \<in> set b \<and> (l1 \<noteq> l2 \<or> u1 \<noteq> u2) 
-          \<longrightarrow> ((u1\<le>l2 \<or> u2\<le>l1) \<and> n1\<noteq>n2))}" by simp
+         (\<forall>n1 n2 l1 l2 u1 u2. (n1, l1, u1) \<in> set b \<and> (n2, l2, u2) \<in> set b \<and> (l1 \<noteq> l2 \<or> u1 \<noteq> u2 \<or> n1\<noteq>n2) 
+          \<longrightarrow> (u1\<le>l2 \<or> u2\<le>l1))}" by simp
   then show ?thesis by blast
 qed
 setup_lifting type_definition_blocks
 
 fun intv :: "loc \<Rightarrow> (loc\<times>loc) \<Rightarrow> bool" where
   "intv x (l,u) = (l\<le>x \<and> x<u)"
+
+definition overlaps_with :: "block \<Rightarrow> block \<Rightarrow> bool" where
+  "overlaps_with b1 b2 \<equiv> (case b1 of (_,l1,u1) \<Rightarrow> case b2 of (_,l2,u2) \<Rightarrow>
+    \<exists>x. intv x (l1,u1) \<and> intv x (l2,u2))"
+
+lemma overlaps_alt: "overlaps_with (n1,l1,u1) (n2,l2,u2) \<longleftrightarrow> l1<u1 \<and> l2<u2 \<and> 
+  ((l1<u2\<and>l2\<le>l1) \<or> (l2<u1\<and>l1\<le>l2))"
+  by (auto simp: overlaps_with_def)
 
 type_synonym pre_config = "stack \<times> blocks \<times> memory"
 
@@ -116,12 +151,45 @@ fun from_block :: "loc \<Rightarrow> block list \<Rightarrow> block option" wher
   "from_block l [] = None"
 | "from_block l ((n,lu)#bs) = (if intv l lu then Some (n,lu) else from_block l bs)"
 
-lemma "\<lbrakk>from_block x (raw_blocks (get_blocks c)) = Some (n,l,u); offset x y<u\<rbrakk>
-  \<Longrightarrow> access (get_memory c) x y \<noteq> None"
-proof
-  assume assms: "from_block x (raw_blocks (get_blocks c)) = Some (n, l, u)" "offset x y < u" 
-    "access (get_memory c) x y = None"
-  show False sorry
+lemma from_block_intv: "from_block x B = Some (n,l,u) \<Longrightarrow> intv x (l,u)"
+  apply (induction B) apply (auto split: option.splits)
+   by (metis Pair_inject option.inject)+
+
+lemma intv_from_block: "\<exists>n l u. intv x (l,u) \<and> (n,l,u) \<in> set B \<Longrightarrow> from_block x B \<noteq> None"
+  apply (induction B) by auto blast+
+
+lemma from_block_set: "from_block x B = Some (n,l,u) \<Longrightarrow> (n,l,u) \<in> set B"
+  apply (induction B) by auto (metis Pair_inject option.inject)+
+
+lemma blocks_non_overlapping:
+  "\<lbrakk>(n1,l1,u1)\<in>set (raw_blocks B); (n2,l2,u2)\<in>set (raw_blocks B); intv z (l1,u1); intv z (l2,u2)\<rbrakk>
+    \<Longrightarrow> (n1,l1,u1) = (n2,l2,u2)"
+proof -
+  assume assms: "(n1,l1,u1)\<in>set (raw_blocks B)" "(n2,l2,u2)\<in>set (raw_blocks B)"
+    "intv z (l1, u1)" "intv z (l2, u2)"
+  with assms have ineq: "l1 \<le> z" "l2 \<le> z" "z < u1" "z < u2" by auto
+  {
+    assume "(n1,l1,u1) \<noteq> (n2,l2,u2)"
+    with assms(1,2) raw_blocks have "(u1\<le>l2 \<or> u2\<le>l1)" by blast
+    with ineq have False by auto
+  }
+  then show ?thesis by auto
+qed
+
+lemma offset_in_block: "\<lbrakk>from_block x (raw_blocks B) = Some (n,l,u); offset x y<u; x\<le>offset x y\<rbrakk>
+  \<Longrightarrow> from_block (offset x y) (raw_blocks B) = Some (n,l,u)"
+proof (induction y arbitrary: x)
+  case 0
+  then show ?case by simp
+next
+  case (Suc y)
+  with from_block_intv[OF Suc(2)] have "l \<le> offset x (Suc y)" by auto
+  with Suc(3) have intv_offset: "intv (offset x (Suc y)) (l,u)" by simp
+  then have "from_block (offset x (Suc y)) (raw_blocks B) \<noteq> None"
+    using intv_from_block from_block_set[OF Suc(2)] by blast
+  then obtain n' l' u' where "from_block (offset x (Suc y)) (raw_blocks B) = Some (n',l',u')" by auto
+  with blocks_non_overlapping[OF from_block_set[OF Suc(2)] from_block_set[OF this] intv_offset from_block_intv[OF this]]
+    show ?case  by simp
 qed
 
 fun bounds :: "block list \<Rightarrow> loc \<Rightarrow> (loc\<times>loc)" where
@@ -135,8 +203,8 @@ lemma bounds_neq_zero: "fst (bounds b l) \<noteq> 0 \<Longrightarrow> intv l (bo
   by (induction rule: bounds.induct) auto
 
 lemma sublist_raw_blocks: "b#bs = raw_blocks B \<Longrightarrow> bs \<in> {b. (\<forall>n l u. (n, l, u) \<in> set b \<longrightarrow> l < u \<and> l \<noteq> 0) \<and>
-         (\<forall>n1 n2 l1 l2 u1 u2. (n1, l1, u1) \<in> set b \<and> (n2, l2, u2) \<in> set b \<and> (l1 \<noteq> l2 \<or> u1 \<noteq> u2) 
-          \<longrightarrow> ((u1\<le>l2 \<or> u2\<le>l1) \<and> n1\<noteq>n2))}" (is "_ \<Longrightarrow> _ \<in> {b. ?block_inv b}")
+         (\<forall>n1 n2 l1 l2 u1 u2. (n1, l1, u1) \<in> set b \<and> (n2, l2, u2) \<in> set b \<and> (l1 \<noteq> l2 \<or> u1 \<noteq> u2 \<or> n1\<noteq>n2) 
+          \<longrightarrow> (u1\<le>l2 \<or> u2\<le>l1))}" (is "_ \<Longrightarrow> _ \<in> {b. ?block_inv b}")
 proof -
   assume "b#bs = raw_blocks B"
   with blocks.raw_blocks have "b#bs \<in> {b. ?block_inv b}" by presburger
@@ -203,8 +271,8 @@ proof (auto)
       by (auto simp: base.rep_eq)
     moreover from bounds_neq_zero_in bl'0 obtain n' where n': "(n', bounds (raw_blocks B) l') \<in> set (raw_blocks B)" 
       by (auto simp: base.rep_eq)
-    ultimately have "(snd (bounds (raw_blocks B) l)\<le>fst (bounds (raw_blocks B) l') \<or> snd (bounds (raw_blocks B) l')\<le>fst (bounds (raw_blocks B) l)) 
-      \<and> n\<noteq>n'" using blocks.raw_blocks by (smt (verit, ccfv_threshold) mem_Collect_eq prod.collapse)
+    ultimately have "(snd (bounds (raw_blocks B) l)\<le>fst (bounds (raw_blocks B) l') \<or> snd (bounds (raw_blocks B) l')\<le>fst (bounds (raw_blocks B) l))" 
+        using blocks.raw_blocks by (smt (verit, ccfv_threshold) mem_Collect_eq prod.collapse)
     with assms(2) have "snd (bounds (raw_blocks B) l)\<le>fst (bounds (raw_blocks B) l')" by (auto simp:  base.rep_eq ende.rep_eq)
     with assms(3)[unfolded ende.rep_eq] have "snd (bounds (raw_blocks B) l') \<le>  fst (bounds (raw_blocks B) l')" by simp
     with n' blocks.raw_blocks[of B] have False using base.rep_eq bl'0 blocks_axiom1 ende.rep_eq order.trans by fastforce
@@ -221,8 +289,8 @@ next
       by (auto simp: base.rep_eq)
     moreover from bounds_neq_zero_in bl'0 obtain n' where n': "(n', bounds (raw_blocks B) l') \<in> set (raw_blocks B)" 
       by (auto simp: base.rep_eq)
-    ultimately have "(snd (bounds (raw_blocks B) l)\<le>fst (bounds (raw_blocks B) l') \<or> snd (bounds (raw_blocks B) l')\<le>fst (bounds (raw_blocks B) l)) 
-      \<and> n\<noteq>n'" using blocks.raw_blocks by (smt (verit, ccfv_threshold) mem_Collect_eq prod.collapse)
+    ultimately have "(snd (bounds (raw_blocks B) l)\<le>fst (bounds (raw_blocks B) l') \<or> snd (bounds (raw_blocks B) l')\<le>fst (bounds (raw_blocks B) l))"
+      using blocks.raw_blocks by (smt (verit, ccfv_threshold) mem_Collect_eq prod.collapse)
     with assms(2) have "snd (bounds (raw_blocks B) l)\<le>fst (bounds (raw_blocks B) l')" by (auto simp:  base.rep_eq ende.rep_eq)
     with assms(3)[unfolded ende.rep_eq] have "snd (bounds (raw_blocks B) l') \<le>  fst (bounds (raw_blocks B) l')" by simp
     with n' blocks.raw_blocks[of B] have False using base.rep_eq bl'0 blocks_axiom1 ende.rep_eq order.trans by fastforce
@@ -239,8 +307,8 @@ next
       by (auto simp: base.rep_eq)
     moreover from bounds_neq_zero_in bl'0 obtain n' where "(n', bounds (raw_blocks B) l') \<in> set (raw_blocks B)" 
       by (auto simp: base.rep_eq)
-    ultimately have "(snd (bounds (raw_blocks B) l)\<le>fst (bounds (raw_blocks B) l') \<or> snd (bounds (raw_blocks B) l')\<le>fst (bounds (raw_blocks B) l)) 
-      \<and> n\<noteq>n'" using blocks.raw_blocks by (smt (verit, ccfv_threshold) mem_Collect_eq prod.collapse)
+    ultimately have "(snd (bounds (raw_blocks B) l)\<le>fst (bounds (raw_blocks B) l') \<or> snd (bounds (raw_blocks B) l')\<le>fst (bounds (raw_blocks B) l))"
+      using blocks.raw_blocks by (smt (verit, ccfv_threshold) mem_Collect_eq prod.collapse)
     with assms(2) have "snd (bounds (raw_blocks B) l')\<le>fst (bounds (raw_blocks B) l)" by (auto simp:  base.rep_eq ende.rep_eq)
     with assms(3)[unfolded ende.rep_eq] have "snd (bounds (raw_blocks B) l) \<le>  fst (bounds (raw_blocks B) l)" by simp
     with n blocks.raw_blocks[of B] have False using base.rep_eq bl0 blocks_axiom1 ende.rep_eq order.trans by fastforce
@@ -257,8 +325,8 @@ next
       by (auto simp: base.rep_eq)
     moreover from bounds_neq_zero_in bl'0 obtain n' where "(n', bounds (raw_blocks B) l') \<in> set (raw_blocks B)" 
       by (auto simp: base.rep_eq)
-    ultimately have "(snd (bounds (raw_blocks B) l)\<le>fst (bounds (raw_blocks B) l') \<or> snd (bounds (raw_blocks B) l')\<le>fst (bounds (raw_blocks B) l)) 
-      \<and> n\<noteq>n'" using blocks.raw_blocks by (smt (verit, ccfv_threshold) mem_Collect_eq prod.collapse)
+    ultimately have "(snd (bounds (raw_blocks B) l)\<le>fst (bounds (raw_blocks B) l') \<or> snd (bounds (raw_blocks B) l')\<le>fst (bounds (raw_blocks B) l))"
+      using blocks.raw_blocks by (smt (verit, ccfv_threshold) mem_Collect_eq prod.collapse)
     with assms(2) have "snd (bounds (raw_blocks B) l')\<le>fst (bounds (raw_blocks B) l)" by (auto simp:  base.rep_eq ende.rep_eq)
     with assms(3)[unfolded ende.rep_eq] have "snd (bounds (raw_blocks B) l) \<le>  fst (bounds (raw_blocks B) l)" by simp
     with n blocks.raw_blocks[of B] have False using base.rep_eq bl0 blocks_axiom1 ende.rep_eq order.trans by fastforce
